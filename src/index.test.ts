2db0e275fcc80165dd337fcfc72784bb8668388e
Noah Gentile <noah.gentile@gmail.com>
2023-03-10 13:06:23 -0800

    initial commit

---
+++ src/index.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, it, beforeAll, afterAll } from "vitest";
+import { unstable_dev, type UnstableDevWorker } from "wrangler";
+
+describe("Worker", () => {
+	let worker: UnstableDevWorker;
+
+	beforeAll(async () => {
+		worker = await unstable_dev("src/index.ts", {
+			experimental: { disableExperimentalWarning: true },
+		});
+	});
+
+	afterAll(async () => {
+		await worker.stop();
+	});
+
+	it("should return Hello World", async () => {
+		const resp = await worker.fetch();
+		if (resp) {
+			const text = await resp.text();
+			expect(text).toMatchInlineSnapshot(`"Hello World!"`);
+		}
+	});
+});
