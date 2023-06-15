5fbc00da0f9663cb4ff02a801b333137caa7cc69
Noah Gentile <noah.gentile@gmail.com>
2023-06-14 20:13:24 -0700

    feat(preview)!: migrate to `@sanity/preview-kit@2`

---
+++ src/preview/PreviewProvider.tsx
@@ -0,0 +1,13 @@
+import {GroqStoreProvider, type GroqStoreProviderProps} from '@sanity/preview-kit/groq-store'
+import type {ReactNode} from 'react'
+
+export type PreviewProviderProps = GroqStoreProviderProps & {children: ReactNode}
+
+export function PreviewProvider(props: PreviewProviderProps) {
+  const {children, ...rest} = props
+
+  return <GroqStoreProvider {...rest}>{children}</GroqStoreProvider>
+}
+
+/** interoperability with `React.lazy` */
+export default PreviewProvider
