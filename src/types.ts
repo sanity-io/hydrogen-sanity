eae1ca318ef3c86e85fe4c2a5540e8ce7e3ce422
Noah Gentile <noah.gentile@gmail.com>
2024-05-03 15:43:30 -0700

    feat: use `withCache`

--- src/types.ts
+++ src/types.ts
@@ -2,23 +2,6 @@ import type {CacheShort, HydrogenSession} from '@shopify/hydrogen'
 
 import type {Sanity} from './loader'
 
-interface ExecutionContext {
-  waitUntil(promise: Promise<any>): void
-}
-
-export interface EnvironmentOptions {
-  /**
-   * A Cache API instance.
-   * @see https://developer.mozilla.org/en-US/docs/Web/API/Cache
-   */
-  cache: Cache
-  /**
-   * A runtime utility for serverless environments
-   * @see https://developers.cloudflare.com/workers/runtime-apis/fetch-event/#waituntil
-   */
-  waitUntil: ExecutionContext['waitUntil']
-}
-
 /** @see https://shopify.dev/docs/custom-storefronts/hydrogen/data-fetching/cache#caching-strategies */
 export type CachingStrategy = ReturnType<typeof CacheShort>
 
