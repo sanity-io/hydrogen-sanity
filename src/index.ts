7834904648b661f85ad043cf5112c87f4c1c4402
Noah Gentile <noah.gentile@gmail.com>
2024-05-03 21:26:01 -0700

    feat: export visual editing component

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './client'
 export {createSanityLoader, type Sanity} from './loader'
+export {default as groq} from 'groq'
---
+++ src/visual-editing/index.ts
@@ -0,0 +1,6 @@
+export {VisualEditing} from './VisualEditing'
+export {
+  type CreateDataAttribute,
+  createDataAttribute,
+  type CreateDataAttributeProps,
+} from '@sanity/visual-editing/create-data-attribute'
