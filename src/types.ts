e7e70ffc3c15c52eec995aca9805806e62cb9711
Noah Gentile <noah.gentile@gmail.com>
2024-05-06 09:05:37 -0700

    fix: don't rely on environment variable naming for route

--- src/types.ts
+++ src/types.ts
@@ -5,19 +5,11 @@ import type {Sanity} from './loader'
 /** @see https://shopify.dev/docs/custom-storefronts/hydrogen/data-fetching/cache#caching-strategies */
 export type CachingStrategy = ReturnType<typeof CacheShort>
 
-type EnvLike = {
-  SANITY_PROJECT_ID: string
-  SANITY_DATASET: string
-  SANITY_API_VERSION: string
-  SANITY_API_TOKEN: string
-}
-
 declare module '@shopify/remix-oxygen' {
   /**
    * Declare local additions to the Remix loader context.
    */
   export interface AppLoadContext {
-    env: EnvLike
     session: HydrogenSession
     sanity: Sanity
   }
