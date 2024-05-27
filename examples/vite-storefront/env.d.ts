333d2de29f06e3e78c41918891f6b6eacf628011
Noah Gentile <noah.gentile@gmail.com>
2024-05-26 19:35:40 -0700

    docs: add storefront examples

--- examples/vite-storefront/env.d.ts
+++ examples/vite-storefront/env.d.ts
@@ -30,6 +30,11 @@ declare global {
     PUBLIC_STOREFRONT_ID: string;
     PUBLIC_CUSTOMER_ACCOUNT_API_CLIENT_ID: string;
     PUBLIC_CUSTOMER_ACCOUNT_API_URL: string;
+
+    PUBLIC_SANITY_PROJECT_ID: string;
+    PUBLIC_SANITY_DATASET?: string;
+    PUBLIC_SANITY_API_VERSION?: string;
+    SANITY_PREVIEW_TOKEN: string;
   }
 }
 
