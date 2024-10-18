96117ec8aaacbeaf1f52a2a5905d2016b6cf273d
Noah Gentile <noah.gentile@gmail.com>
2024-10-18 14:40:42 -0700

    fix: use Hydrogen caching strategy type

--- package/src/types.ts
+++
@@ -1,4 +0,0 @@
-import type {CacheShort} from '@shopify/hydrogen'
-
-/** @see https://shopify.dev/docs/custom-storefronts/hydrogen/data-fetching/cache#caching-strategies */
-export type CachingStrategy = ReturnType<typeof CacheShort>
