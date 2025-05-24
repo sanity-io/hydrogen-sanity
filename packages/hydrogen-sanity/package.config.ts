cb9ce42829065983facc0a09dae520dac6c8f68b
Noah Gentile <noah.gentile@gmail.com>
2025-05-24 12:42:24 -0700

    feat: compile with React Compiler

--- packages/hydrogen-sanity/package.config.ts
+++ packages/hydrogen-sanity/package.config.ts
@@ -30,10 +30,12 @@ export default defineConfig({
 
   // external: ['rxjs'],
 
+  babel: {reactCompiler: true},
+  reactCompilerOptions: {target: '18'},
+
   // Remove this block to enable strict export validation
   extract: {
     rules: {
-      'ae-forgotten-export': 'off',
       'ae-incompatible-release-tags': 'off',
       'ae-internal-missing-underscore': 'off',
       'ae-missing-release-tag': 'off',
