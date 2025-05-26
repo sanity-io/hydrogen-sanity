8d6ec0150e15d7ef35fd1148d2b83d6deda2b877
Noah Gentile <noah.gentile@gmail.com>
2025-05-26 11:52:56 -0700

    chore: adjust turbo

--- examples/studio/sanity.config.ts
+++ examples/studio/sanity.config.ts
@@ -1,5 +1,7 @@
 import {defineStudioConfig} from '@repo/sanity-config'
 
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID!
+
 export default defineStudioConfig({
-  projectId: 'ue589xuu',
+  projectId,
 })
