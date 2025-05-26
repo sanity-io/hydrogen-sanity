82cd6dc7bd38e1f22d32cb22e218d1dae33913b6
Noah Gentile <noah.gentile@gmail.com>
2025-05-26 16:17:13 -0700

    chore: use JS `semantic-release` configuration

---
+++ packages/hydrogen-sanity/release.config.mjs
@@ -0,0 +1,22 @@
+export default {
+  extends: '@sanity/semantic-release-preset',
+  branches: [
+    {
+      name: 'main',
+      channel: 'latest',
+    },
+    // {
+    //   name: 'v4',
+    //   channel: 'v4',
+    //   range: '^4.0.0',
+    // },
+    {
+      name: 'beta',
+      prerelease: true,
+    },
+    {
+      name: 'next',
+      prerelease: true,
+    },
+  ],
+}
