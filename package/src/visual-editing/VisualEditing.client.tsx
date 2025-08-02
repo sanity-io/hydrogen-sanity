60280166bdf2396b55fd3472aeb088fbf1eb9c48
Noah Gentile <noah.gentile@gmail.com>
2025-08-01 21:39:50 -0700

    feat: listen for and submit perspective changes

--- package/src/visual-editing/VisualEditing.client.tsx
+++ package/src/visual-editing/VisualEditing.client.tsx
@@ -1,4 +1,11 @@
-import {VisualEditing} from '@sanity/visual-editing/remix'
+import {isMaybePresentation} from '@sanity/presentation-comlink'
+import {
+  VisualEditing as VisualEditingComponent,
+  type VisualEditingProps,
+} from '@sanity/visual-editing/remix'
+import {type ReactNode, useSyncExternalStore} from 'react'
+
+import PresentationComlink, {type PresentationComlinkProps} from './PresentationComlink.client'
 
 /**
  * Prevent a consumer from importing into a worker/server bundle.
@@ -13,4 +20,34 @@ if (typeof document === 'undefined') {
  * Enables visual editing on the front-end
  * @see https://www.sanity.io/docs/introduction-to-visual-editing
  */
-export default VisualEditing
+export default function VisualEditing(
+  props: VisualEditingProps & PresentationComlinkProps,
+): ReactNode {
+  // eslint-disable-next-line prefer-const
+  let {action, ...visualEditingProps} = props
+
+  if (!action) {
+    console.warn(
+      'No action URL provided for the visual editing component to set a new perspective. Defaulting to "/api/preview".',
+    )
+    action = '/api/preview'
+  }
+
+  const maybePresentation = useSyncExternalStore(
+    noop,
+    () => isMaybePresentation(),
+    () => false,
+  )
+
+  return (
+    <>
+      {maybePresentation && <PresentationComlink action={action} />}
+      <VisualEditingComponent {...visualEditingProps} />
+    </>
+  )
+}
+
+function noop() {
+  // eslint-disable-next-line no-empty-function
+  return () => {}
+}
