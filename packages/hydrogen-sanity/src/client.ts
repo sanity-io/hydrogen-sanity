ea008abd8f1c99c8f93146e3e2f4ae9f03dda696
Noah Gentile <noah.gentile@gmail.com>
2025-08-16 19:26:39 -0700

    feat!: drop `groq` and `client` exports
    BREAKING CHANGE: `hydrogen-sanity` no longer re-exports `@sanity/client` and `groq`. Both are now true peer dependencies and you are expected to install them as direct dependencies of your storefront.

--- packages/hydrogen-sanity/src/client.ts
+++
@@ -1,3 +0,0 @@
-export type * from '@sanity/client'
-export {createClient, SanityClient} from '@sanity/client'
-export {stegaClean} from '@sanity/client/stega'
--- packages/hydrogen-sanity/src/groq.ts
+++
@@ -1,1 +0,0 @@
-export {defineQuery, default as groq} from 'groq'
