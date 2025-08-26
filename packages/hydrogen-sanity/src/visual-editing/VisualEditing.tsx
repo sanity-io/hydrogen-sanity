6ab5a5ae66c0d71812b71290969c725a40cc8b67
Noah Gentile <noah.gentile@gmail.com>
2025-08-26 13:11:41 -0700

    refactor: add `isServer` utility

--- packages/hydrogen-sanity/src/visual-editing/VisualEditing.tsx
+++ packages/hydrogen-sanity/src/visual-editing/VisualEditing.tsx
@@ -1,33 +1,26 @@
 import type {VisualEditingProps} from '@sanity/visual-editing/react-router'
-import {lazy, type ReactElement, Suspense} from 'react'
+import {Fragment, lazy, type ReactElement, Suspense} from 'react'
 
+import {isServer} from '../utils'
 import type {PresentationComlinkProps} from './PresentationComlink.client'
-
-/**
- * Provide a consistent fallback to prevent hydration mismatch errors.
- */
-function VisualEditingFallback(): ReactElement {
-  return <></>
-}
 
 /**
  * If server-side rendering, then return the fallback instead of the heavy dependency.
  */
-const VisualEditingComponent =
-  typeof document === 'undefined'
-    ? VisualEditingFallback
-    : lazy(
-        () =>
-          /**
-           * `lazy` expects the component as the default export
-           * @see https://react.dev/reference/react/lazy
-           */
-          import('./VisualEditing.client'),
-      )
+const VisualEditingComponent = isServer()
+  ? Fragment
+  : lazy(
+      () =>
+        /**
+         * `lazy` expects the component as the default export
+         * @see https://react.dev/reference/react/lazy
+         */
+        import('./VisualEditing.client'),
+    )
 
 export function VisualEditing(props: VisualEditingProps & PresentationComlinkProps): ReactElement {
   return (
-    <Suspense>
+    <Suspense fallback={<Fragment />}>
       <VisualEditingComponent {...props} />
     </Suspense>
   )
