3b72ba926f14801456b3ab01ca658bc73bbe3dfa
Noah Gentile <noah.gentile@gmail.com>
2026-01-16 13:06:27 -0800

    chore(release): simplify v5 branch config to minimal pattern

--- packages/hydrogen-sanity/release.config.mjs
+++ packages/hydrogen-sanity/release.config.mjs
@@ -1,27 +1,4 @@
 export default {
   extends: '@sanity/semantic-release-preset',
-  branches: [
-    {
-      name: 'main',
-      channel: 'latest',
-    },
-    {
-      name: 'beta',
-      prerelease: true,
-    },
-    {
-      name: 'next',
-      prerelease: true,
-    },
-    {
-      name: 'v4',
-      channel: 'v4',
-      range: '4.x.x',
-    },
-    {
-      name: 'v5',
-      channel: 'v5',
-      range: '5.x.x',
-    },
-  ],
+  branches: ['main', {name: 'v5', channel: '5', range: '5.x.x'}],
 }
