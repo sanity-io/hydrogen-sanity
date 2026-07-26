d2bd34f328a5bb0112563c243d8f33a8dda4d955
Noah Gentile <noah.gentile@gmail.com>
2026-07-26 09:47:16 -0700

    chore(storefront): align example storefront with Hydrogen skeleton 2026.4.4

--- examples/storefront/.graphqlrc.ts
+++ examples/storefront/.graphqlrc.ts
@@ -1,6 +1,11 @@
 import type {IGraphQLConfig} from 'graphql-config';
 import {getSchema} from '@shopify/hydrogen-codegen';
 
+/**
+ * GraphQL Config
+ * @see https://the-guild.dev/graphql/config/docs/user/usage
+ * @type {IGraphQLConfig}
+ */
 const graphqlConfig: IGraphQLConfig = {
   projects: {
     default: {
@@ -11,10 +16,13 @@ const graphqlConfig: IGraphQLConfig = {
         '!./app/graphql/**/*.{ts,tsx,js,jsx}',
       ],
     },
+
     customer: {
       schema: getSchema('customer-account'),
       documents: ['./app/graphql/customer-account/*.{ts,tsx,js,jsx}'],
     },
+
+    // Add your own GraphQL projects here for CMS, Shopify Admin API, etc.
   },
 };
 
